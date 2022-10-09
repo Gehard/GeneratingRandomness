7b2f5d29b38296f4a1b3ff6eb579d2ef94d8394d
gehard <gehardnjoro@gmail.com>
2022-10-09 17:39:15 +0300

    Input processing

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -5,3 +5,9 @@ while len(final_string) < 100:
     print(f"Current data length is {len(final_string)}"
           f", {100 - len(final_string)} symbols left")
 print(f"\nFinal data string:\n{final_string}")
+triads = ['000', '001', '010', '011', '100', '101', '110', '111']
+parts = [final_string[index:index + 4] for index in range(0, len(final_string) - 3)]
+for x in triads:
+    res_0 = parts.count(x + '0')
+    res_1 = parts.count(x + '1')
+    print(f'{x}: {res_0}, {res_1}')
